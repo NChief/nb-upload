ec7bbbabdb7a370bf685166bccea42034ef5c973
NChief <nchief.nortv@gmail.com>
2011-05-16 02:25:04 +0200

    higher picesize on torrent

--- nb-upload.pl
+++ nb-upload.pl
@@ -110,7 +110,7 @@ sub login {
 }
 
 sub create_torrent {
-        system("buildtorrent -q -p1 -a http://jalla.com $path $torrent_file_dir/$release.torrent");
+        system("buildtorrent -q -p1 -L 41941304 -a http://jalla.com $path $torrent_file_dir/$release.torrent");
         return $torrent_file_dir."/".$release.".torrent";
 }
 
